509579696ce097c619e8c87ce672c342850284a9
Florent Clarret <florent.clarret@gmail.com>
2018-07-01 16:02:11 +0200

    test(bitfield) : add test to check the constructor with one value

--- src/test/java/fr/florentclarret/bitfield/BitFieldTest.java
+++ src/test/java/fr/florentclarret/bitfield/BitFieldTest.java
@@ -1,8 +1,14 @@
 package fr.florentclarret.bitfield;
 
 import fr.florentclarret.bitfield.enums.WeekDay;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -25,5 +31,22 @@ public class BitFieldTest {
                 final Set<WeekDay> set = null;
                 new BitField<>(set);
             });
+    }
+
+    @TestFactory
+    public Stream<DynamicTest> testConstructorFromArray_WithOneValue() {
+        final Map<WeekDay, Long> weekDays = new HashMap<>();
+        weekDays.put(WeekDay.MONDAY, 1L);
+        weekDays.put(WeekDay.TUESDAY, 2L);
+        weekDays.put(WeekDay.WEDNESDAY, 4L);
+        weekDays.put(WeekDay.THURSDAY, 8L);
+        weekDays.put(WeekDay.FRIDAY, 16L);
+        weekDays.put(WeekDay.SATURDAY, 32L);
+        weekDays.put(WeekDay.SUNDAY, 64L);
+
+        return weekDays.entrySet().stream().map(day -> DynamicTest.dynamicTest(
+                "day[" + day.getKey() + "], expectedBitFieldValue[" + day.getValue() + "]",
+                () -> assertEquals(day.getValue().longValue(),
+                        new BitField<>(day.getKey()).getBitFieldValue())));
     }
 }
