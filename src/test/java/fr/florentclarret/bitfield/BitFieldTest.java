d636ddc6a9e3408506703254701e3ab581c43147
Florent Clarret <florent.clarret@gmail.com>
2018-07-01 16:03:18 +0200

    chore(bitfield) : reorder test cases

--- src/test/java/fr/florentclarret/bitfield/BitFieldTest.java
+++ src/test/java/fr/florentclarret/bitfield/BitFieldTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -24,15 +25,6 @@ public class BitFieldTest {
         assertTrue(weekDaysBitField.getSet().isEmpty());
     }
 
-    @Test
-    public void testConstructorFromSet_WithNullInput() {
-        assertThrows(IllegalArgumentException.class,
-            () -> {
-                final Set<WeekDay> set = null;
-                new BitField<>(set);
-            });
-    }
-
     @TestFactory
     public Stream<DynamicTest> testConstructorFromArray_WithOneValue() {
         final Map<WeekDay, Long> weekDays = new HashMap<>();
@@ -48,5 +40,38 @@ public class BitFieldTest {
                 "day[" + day.getKey() + "], expectedBitFieldValue[" + day.getValue() + "]",
                 () -> assertEquals(day.getValue().longValue(),
                         new BitField<>(day.getKey()).getBitFieldValue())));
+    }
+
+    @TestFactory
+    public Stream<DynamicTest> testConstructorFromSet() {
+        final Map<Set<WeekDay>, Long> map = new HashMap<>();
+        map.put(EnumSet.noneOf(WeekDay.class), 0L);
+        map.put(EnumSet.of(WeekDay.MONDAY), 1L);
+        map.put(EnumSet.of(WeekDay.TUESDAY), 2L);
+        map.put(EnumSet.of(WeekDay.WEDNESDAY), 4L);
+        map.put(EnumSet.of(WeekDay.THURSDAY), 8L);
+        map.put(EnumSet.of(WeekDay.FRIDAY), 16L);
+        map.put(EnumSet.of(WeekDay.SATURDAY), 32L);
+        map.put(EnumSet.of(WeekDay.SUNDAY), 64L);
+        map.put(EnumSet.of(WeekDay.MONDAY, WeekDay.FRIDAY), 17L);
+        map.put(EnumSet.of(WeekDay.SATURDAY, WeekDay.THURSDAY), 40L);
+        map.put(EnumSet.of(WeekDay.SUNDAY, WeekDay.WEDNESDAY), 68L);
+        map.put(EnumSet.of(WeekDay.SUNDAY, WeekDay.WEDNESDAY, WeekDay.THURSDAY), 76L);
+        map.put(EnumSet.of(WeekDay.TUESDAY, WeekDay.SATURDAY, WeekDay.THURSDAY), 42L);
+        map.put(EnumSet.of(WeekDay.MONDAY, WeekDay.TUESDAY, WeekDay.WEDNESDAY, WeekDay.THURSDAY, WeekDay.FRIDAY, WeekDay.SATURDAY, WeekDay.SUNDAY), 127L);
+
+        return map.entrySet().stream().map(element -> DynamicTest.dynamicTest(
+                "days[" + element.getKey() + "], expectedBitFieldValue[" + element.getValue() + "]",
+                () -> assertEquals(element.getValue().longValue(),
+                        new BitField<>(element.getKey()).getBitFieldValue())));
+    }
+
+    @Test
+    public void testConstructorFromSet_WithNullInput() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    final Set<WeekDay> set = null;
+                    new BitField<>(set);
+                });
     }
 }
